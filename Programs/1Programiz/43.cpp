86966b9a203e16b4beddc8e9c1a9947587a230d8
SASWAT KUMAR PANDA <saswatkumarpanda515@gmail.com>
2021-09-21 23:41:10 +0530

    Program to Multiply Two Matrices Using Multi-dimensional Arrays

--- Programs/1Programiz/43.cpp
+++ Programs/1Programiz/43.cpp
@@ -1,4 +1,4 @@
-// C Program to Multiply Two Matrices Using Multi-dimensional Arrays.
+// C Program to Multiply Two Matrices Using Multi-dimensional Arrays
 
 #include <iostream>
 using namespace std;
